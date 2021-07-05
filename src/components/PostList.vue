1f7c5dc7effb09bcdfee4a4ab08b8dd602278edb
dh <kellog_g@naver.com>
2021-07-05 19:00:58 +0900

    add button

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,34 +1,41 @@
 <template>
-  <div>
-    <v-row class="ml-5 mr-5">
+  <div class="ml-5 mr-5">
+    <v-row>
       <v-col cols="2">
         <v-select v-model="keyword" :items="keywords"></v-select>
       </v-col>
-      <v-col col="10">
+      <v-col cols="8">
         <v-text-field color="black" label="검색어를 입력하세요." append-icon="mdi-help-box"></v-text-field>
       </v-col>
+      <v-col class="mt-4" cols="2">
+        <v-btn primary>글쓰기</v-btn>
+      </v-col>
     </v-row>
-    <v-card class="mt-2 ml-5 mr-5">
-      <v-list two-line>
-        <v-list-item-group active-class="white--text" multiple>
-          <template v-for="(post, index) in posts">
-            <v-list-item :to="{ path: '/board/' + post.pno }" :key="post.pno">
-              <v-list-item-content>
-                <v-list-item-title class="font-weight-bold" v-text="post.title"></v-list-item-title>
-                <v-row>
-                  <v-col class="text--secondary" v-text="post.user" cols="2"></v-col>
-                </v-row>
-              </v-list-item-content>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-list two-line>
+            <v-list-item-group active-class="white--text" multiple>
+              <template v-for="(post, index) in posts">
+                <v-list-item :to="{ path: '/board/' + post.pno }" :key="post.pno">
+                  <v-list-item-content>
+                    <v-list-item-title class="font-weight-bold" v-text="post.title"></v-list-item-title>
+                    <v-row>
+                      <v-col class="text--secondary" v-text="post.user" cols="2"></v-col>
+                    </v-row>
+                  </v-list-item-content>
 
-              <v-list-item-action>
-                <v-list-item-action-text v-text="post.modifiedAt"></v-list-item-action-text>
-              </v-list-item-action>
-            </v-list-item>
-            <v-divider v-if="index < posts.length - 1" :key="index"></v-divider>
-          </template>
-        </v-list-item-group>
-      </v-list>
-    </v-card>
+                  <v-list-item-action>
+                    <v-list-item-action-text v-text="post.modifiedAt"></v-list-item-action-text>
+                  </v-list-item-action>
+                </v-list-item>
+                <v-divider v-if="index < posts.length - 1" :key="index"></v-divider>
+              </template>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
 
     <v-row class="mt-2 ml-5 mr-5">
       <v-col cols="2"></v-col>
@@ -53,9 +60,6 @@ export default {
       page: this.page,
       size: this.size,
     });
-  },
-  mounted() {
-    console.log('mount==>' + this.page);
   },
   computed: {
     ...mapState(['posts', 'totalPage']),
